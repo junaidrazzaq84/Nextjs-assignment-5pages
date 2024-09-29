37bd42a06b515f3a766a407418164ec4d4f71528
Junaid Razzaq <142839488+junaidrazzaq84@users.noreply.github.com>
2024-09-29 05:45:08 +0500

    Add files via upload

---
+++ my-app/src/app/about/overview/page.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/app/components/navbar";
+export default function OverView() {
+  return (
+    <div>
+      <h1>This is Overview Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas,
+        vitae recusandae quia ut rerum dolore dicta nihil saepe quam eos ratione
+        ab distinctio voluptatum vero illum ullam excepturi placeat iste. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eius
+        debitis quibusdam illum perspiciatis non modi, placeat quasi soluta
+        ducimus quam asperiores magnam porro quae tempora sed animi, sint hic.
+      </p>
+      <Navbar />
+      <h2>Prepared By Junaid A.Razzaq</h2>
+    </div>
+  );
+}
