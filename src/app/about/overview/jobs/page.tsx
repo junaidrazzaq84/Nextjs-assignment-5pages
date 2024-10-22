af5b7f22b65a574baa17e358b940fdc791eb87b5
Junaid Razzaq <142839488+junaidrazzaq84@users.noreply.github.com>
2024-10-23 01:09:57 +0500

    Add files via upload

---
+++ src/app/about/overview/jobs/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Jobs() {
+  return (
+    <div>
+      <h1 className="bg-slate-400 text-red-950 font-bold border-8 rounded-full border-blue-950 p-2 m-3">
+        This is Jobs Page
+      </h1>
+      <p className="bg-gray-400 text-justify font-bold border-8 border-blue-800 text-emerald-950">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas,
+        vitae recusandae quia ut rerum dolore dicta nihil saepe quam eos ratione
+        ab distinctio voluptatum vero illum ullam excepturi placeat iste. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eius
+        debitis quibusdam illum perspiciatis non modi, placeat quasi soluta
+        ducimus quam asperiores magnam porro quae tempora sed animi, sint hic.
+      </p>
+    </div>
+  );
+}
